fc2e75ff8233ce6d7e661dc6da0f44c4f7561d39
Andrii Kachur <andrii_kachur@epam.com>
2022-01-12 18:36:06 +0200

    Add RecursiveTask to Java7ParallelAggregator for sum method

--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
@@ -3,11 +3,33 @@ package com.epam.cdp.m2.hw2.aggregator;
 import javafx.util.Pair;
 
 import java.util.List;
+import java.util.concurrent.RecursiveTask;
 
-public class Java7ParallelAggregator implements Aggregator {
+public class Java7ParallelAggregator extends RecursiveTask<Integer> implements Aggregator  {
+
+    private List<Integer> numbers;
+
+    Java7ParallelAggregator(List<Integer> numbers){
+        this.numbers = numbers;
+    }
 
     @Override
-    public int sum(List<Integer> numbers) {
+    protected Integer compute() {
+        if(numbers.size() <= 1){
+            return numbers.get(0);
+        }
+        else if(numbers.size() <= 2){
+            return numbers.get(0) + numbers.get(1);
+        }
+        Java7ParallelAggregator firstHalfList = new Java7ParallelAggregator(numbers.subList(0, numbers.size()/2));
+        Java7ParallelAggregator secondHalfList = new Java7ParallelAggregator(numbers.subList(numbers.size()/2, numbers.size()));
+        firstHalfList.fork();
+        secondHalfList.fork();
+        return firstHalfList.join() + secondHalfList.join();
+    }
+
+    @Override
+    public int sum(List<Integer> list) {
         throw new UnsupportedOperationException();
     }
 
--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Main.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Main.java
@@ -1,16 +1,23 @@
 package com.epam.cdp.m2.hw2.aggregator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         Java7Aggregator java7Aggregator = new Java7Aggregator();
         Java8Aggregator java8Aggregator = new Java8Aggregator();
         System.out.println(java7Aggregator.sum(List.of(1, 3, 4, 2)));
-        System.out.println(java7Aggregator.getMostFrequentWords(List.of("b", "a", "a", "b"), 2));
-        System.out.println(java7Aggregator.getDuplicates(List.of("go", "lol", "stop", "GO", "AA", "aa"), 1));
-        System.out.println(java8Aggregator.sum(List.of(1, 3, 4, 2)));
-        System.out.println(java8Aggregator.getMostFrequentWords(List.of("bla", "ko", "bla", "lomo", "lomo", "bla"), 2));
-        System.out.println(java8Aggregator.getDuplicates(List.of("go", "lol", "stop", "GO", "aa", "aa"), 3));
+//        System.out.println(java7Aggregator.getMostFrequentWords(List.of("b", "a", "a", "b"), 2));
+//        System.out.println(java7Aggregator.getDuplicates(List.of("go", "lol", "stop", "GO", "AA", "aa"), 1));
+//        System.out.println(java8Aggregator.sum(List.of(1, 3, 4, 2)));
+//        System.out.println(java8Aggregator.getMostFrequentWords(List.of("bla", "ko", "bla", "lomo", "lomo", "bla"), 2));
+//        System.out.println(java8Aggregator.getDuplicates(List.of("go", "lol", "stop", "GO", "aa", "aa"), 3));
+
+        Java7ParallelAggregator parallelAggregator = new Java7ParallelAggregator(List.of(1, 2, 3, 4, 5));
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        System.out.println(forkJoinPool.invoke(parallelAggregator));
+
     }
 }
